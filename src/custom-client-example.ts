443c444b20ad3d2f261432182dfc6947b9bc2412
ppedziwiatr <peter@redstone.finance>
2021-08-24 15:53:43 +0200

    feat: switch to redstone-smartweave lib

--- src/custom-client-example.ts
+++ src/custom-client-example.ts
@@ -1,5 +1,4 @@
-import * as fs from 'fs';
-import Arweave from 'arweave';
+import Arweave from "arweave";
 import {
   CacheableExecutorFactory,
   CacheableStateEvaluator,
@@ -13,8 +12,7 @@ import {
   LexicographicalInteractionsSorter,
   MemBlockHeightSwCache,
   MemCache,
-} from 'smartweave/lib/v2';
-
+} from "redstone-smartweave";
 
 // note: this ofc. should be imported from the given SWC source code.
 interface ProvidersRegistryState extends EvolveCompatibleState {
@@ -22,57 +20,73 @@ interface ProvidersRegistryState extends EvolveCompatibleState {
 }
 
 export function timeout(ms: number): Promise<any> {
-  return new Promise(resolve => setTimeout(() => resolve('timeout'), ms));
+  return new Promise((resolve) => setTimeout(() => resolve("timeout"), ms));
 }
 
 async function customClientExample() {
-
   const arweave = Arweave.init({
-    host: 'arweave.net',// Hostname or IP address for a Arweave host
-    port: 443,          // Port
-    protocol: 'https',  // Network protocol http or https
-    timeout: 20000,     // Network request timeouts in milliseconds
-    logging: false,     // Enable network request logging
+    host: "arweave.net", // Hostname or IP address for a Arweave host
+    port: 443, // Port
+    protocol: "https", // Network protocol http or https
+    timeout: 20000, // Network request timeouts in milliseconds
+    logging: false, // Enable network request logging
   });
 
-  console.log('arweave created');
+  console.log("arweave created");
 
   // in real world usage -
   // you could modify original contract's code (eg. add some console.logs for debugging) and - for example -
   // read it here from file
-  const changedSrc =
-    `function handle(state, action) {
+  const changedSrc = `function handle(state, action) {
    console.log("\\n ===== Hello World from the new source:", SmartWeave.transaction.id);
    return {state}
   }`;
 
   // CacheableExecutorFactory is a wrapper over base ExecutorFactory that adds caching capabilities.
-  const cacheableExecutorFactory = new CacheableExecutorFactory<any, any>(arweave, new HandlerExecutorFactory(arweave), new MemCache());
+  const cacheableExecutorFactory = new CacheableExecutorFactory<any, any>(
+    arweave,
+    new HandlerExecutorFactory(arweave),
+    new MemCache()
+  );
 
   // DebuggableExecutorFactory is a wrapper over base ExecutorFactory that adds a feature of substituting contract's code.
   // note: this will effectively substitute original contract's source code with code from `changedSrc` variable.
-  const debuggableExecutorFactory = new DebuggableExecutorFactory(cacheableExecutorFactory, {
-    "OrO8n453N6bx921wtsEs-0OCImBLCItNU5oSbFKlFuU": changedSrc
-  });
+  const debuggableExecutorFactory = new DebuggableExecutorFactory(
+    cacheableExecutorFactory,
+    {
+      "OrO8n453N6bx921wtsEs-0OCImBLCItNU5oSbFKlFuU": changedSrc,
+    }
+  );
 
   // Configuring HandlerBaseSwcClient from scratch.
   const swcClient = new HandlerBasedSwcClient(
     arweave,
-    new ContractDefinitionLoader<ProvidersRegistryState>(arweave, new MemCache()),
+    new ContractDefinitionLoader<ProvidersRegistryState>(
+      arweave,
+      new MemCache()
+    ),
     new ContractInteractionsLoader(arweave),
     debuggableExecutorFactory,
-    new CacheableStateEvaluator(arweave, new MemBlockHeightSwCache<EvalStateResult<ProvidersRegistryState>>()),
-    new LexicographicalInteractionsSorter(arweave));
+    new CacheableStateEvaluator(
+      arweave,
+      new MemBlockHeightSwCache<EvalStateResult<ProvidersRegistryState>>()
+    ),
+    new LexicographicalInteractionsSorter(arweave)
+  );
 
-  console.log('swcClient created');
+  console.log("swcClient created");
 
-  const providersRegistryContractTxId = "OrO8n453N6bx921wtsEs-0OCImBLCItNU5oSbFKlFuU";
+  const providersRegistryContractTxId =
+    "OrO8n453N6bx921wtsEs-0OCImBLCItNU5oSbFKlFuU";
 
   // Reading contract's state using new client.
-  const { state, validity } = await swcClient.readState(providersRegistryContractTxId);
+  const { state, validity } = await swcClient.readState(
+    providersRegistryContractTxId
+  );
 
-  console.log('Result', {
-    state, validity,
+  console.log("Result", {
+    state,
+    validity,
   });
 }
 
--- src/file-cache-client.ts
+++ src/file-cache-client.ts
@@ -1,33 +1,37 @@
-import Arweave from 'arweave';
-import { SwClientFactory } from 'smartweave/lib/v2';
-import path from 'path';
+import Arweave from "arweave";
+import path from "path";
+import { SwClientFactory } from "redstone-smartweave";
 
 async function memCacheClient() {
-
   const arweave = Arweave.init({
-    host: 'arweave.net',// CloudFront based Arweave cache
-    port: 443,          // Port
-    protocol: 'https',  // Network protocol http or https
-    timeout: 20000,     // Network request timeouts in milliseconds
-    logging: false,     // Enable network request logging
+    host: "arweave.net", // CloudFront based Arweave cache
+    port: 443, // Port
+    protocol: "https", // Network protocol http or https
+    timeout: 20000, // Network request timeouts in milliseconds
+    logging: false, // Enable network request logging
   });
 
-  console.log('arweave created');
+  console.log("arweave created");
 
   // using SwClientFactory to quickly obtain fully configured, file-cacheable SwcClient instance
   // see TSDoc for BsonFileBlockHeightSwCache class.
-  const swcClient = SwClientFactory.fileCacheClient(arweave, path.join(__dirname, 'cache'));
-  console.log('swcClient created');
+  const swcClient = SwClientFactory.fileCacheClient(
+    arweave,
+    path.join(__dirname, "cache")
+  );
+  console.log("swcClient created");
 
-  const providersRegistryContractTxId = "OrO8n453N6bx921wtsEs-0OCImBLCItNU5oSbFKlFuU";
+  const providersRegistryContractTxId =
+    "OrO8n453N6bx921wtsEs-0OCImBLCItNU5oSbFKlFuU";
 
   // Reading contract's state using new client in the loop
   for (let i = 0; i < 5; i++) {
     console.time(`Contract call ${i + 1}`);
-    const { state, validity } = await swcClient.readState(providersRegistryContractTxId);
+    const { state, validity } = await swcClient.readState(
+      providersRegistryContractTxId
+    );
     console.timeEnd(`Contract call ${i + 1}`);
   }
-
 }
 
 memCacheClient().catch((e) => {
