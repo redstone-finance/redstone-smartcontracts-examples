833beb019dcd2bd0e7e785eda841b0a5217e504b
hatskier <suvorovalex1997@gmail.com>
2021-09-09 14:21:11 +0200

    randimg generation improved, colors added

--- src/contracts/loot/contract.js
+++ src/contracts/loot/contract.js
@@ -1,21 +1,28 @@
-export function handle(state, action) {
+export async function handle(state, action) {
 
-  const MATERIALS = ["golden", "wooden", "silvern", "fiery", "diamond"];
+  const COLORS = ["green", "red", "yellow", "blue", "black", "brown", "pink"];
+  const MATERIALS = ["golden", "wooden", "silvern", "fiery", "diamond", "platinum", "palladium"];
   const ITEMS = ["sword", "shield", "robe", "stone", "crown", "katana", "gragon", "ring"];
 
-  // This function removes all non-digits from string
-  // Then it converts to number
-  // If the result string is empty, it returns 0
-  function getNumberFromString(str) {
-    return str.replace(/\D/g, "") || 0;
+  function bigIntFromBytes(byteArr) {
+    const hexString = byteArr.toString("hex");
+    return BigInt("0x" + hexString) % BigInt(Number.MAX_SAFE_INTEGER);
   }
 
-  function getRandomIntNumber(max) {
-    const bigNumber = SmartWeave.block.timestamp
-      + SmartWeave.block.height
-      + getNumberFromString(SmartWeave.transaction.id)
-      + getNumberFromString(action.caller);
-    return Math.round(bigNumber % (max + 1));
+  // This function calculates a pseudo-random int value,
+  // which is less then the `max` argument.
+  // Note! This method should not be used more than once
+  // in a single contract interaction
+  async function getRandomIntNumber(max) {
+    const pseudoRandomData = SmartWeave.utils.stringToBuffer(
+      SmartWeave.block.height
+      + SmartWeave.block.timestamp
+      + SmartWeave.transaction.id
+      + action.caller
+    );
+    const hashBytes = await SmartWeave.crypto.hash(pseudoRandomData);
+    const randomBigInt = bigIntFromBytes(hashBytes);
+    return Number(randomBigInt % BigInt(max));
   }
 
   switch (action.input.function) {
@@ -38,16 +45,27 @@ export function handle(state, action) {
       }
 
     case "generate":
-      const assetIndex = getRandomIntNumber(MATERIALS.length * ITEMS.length);
-      const materialIndex = assetIndex % MATERIALS.length;
-      const itemIndex = Math.floor(assetIndex / MATERIALS.length);
-      const asset = MATERIALS[materialIndex] + " " + ITEMS[itemIndex];
+      const allAssetsCount = COLORS.length * MATERIALS.length * ITEMS.length;
+      const assetIndex = await getRandomIntNumber(allAssetsCount);
+      const colorIndex = assetIndex % COLORS.length;
+      const materialIndex = Math.floor(assetIndex / COLORS.length) % MATERIALS.length;
+      const itemIndex = Math.floor(assetIndex / (COLORS.length * MATERIALS.length)) % ITEMS.length;
+      const asset = COLORS[colorIndex] + " " + MATERIALS[materialIndex] + " " + ITEMS[itemIndex];
       if (!state.assets[asset]) {
         // If the asset wasn't generated yet, create it and give it to caller
         state.assets[asset] = action.caller;
       }
       return { state };
 
+    case "transfer":
+      const toAddress = action.input.data.to;
+      const asset = action.input.data.asset;
+      if (state.assets[asset] !== action.caller) {
+        throw new ContractError("Can not transfer asset that doesn't belong to you");
+      }
+      state.assets[asset] = toAddress;
+      return { state };
+    
     default:
       throw new ContractError(
         `Unsupported contract function: ${functionName}`);
